dcccb1ff7db6dafb3248fc60247c08b8a7d3e9e3
Daniel Cowpertwait <danielcowpertwait@gmail.com>
2020-01-28 05:15:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,5 +2,8 @@ def reverse_each_word(sentence)
   each_word = sentence.split(" ")
   words_reversed = words.collect |word|
   word.reverse
-  words_reversed 
+  words_reversed.join(" ")
+end
+
+
   
