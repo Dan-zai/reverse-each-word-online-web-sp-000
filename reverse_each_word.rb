7ed2e8ac007318fe4b44a5554b644bcaab963caf
Daniel Cowpertwait <danielcowpertwait@gmail.com>
2020-01-28 05:25:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,12 @@
-def reverse_each_word(sentence)
-  each_word = sentence.split(" ")
-  words_reversed = each_word.collect {|word| word.reverse}
+def reverse_each_word(sentence) #takes the sentence as an argument for method
+  
+  individual_word = sentence.split(" ") #splits each word using operator on string and places it within empty string argument
+  
+  words_reversed = individual_word.collect {|word| word.reverse}
+  #collects each word, assigns a point in the collection and reverses
+  
   words_reversed.join(" ")
+  #joins em back up
 end
 
 
